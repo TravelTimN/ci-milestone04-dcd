00e2a0e7c206a848b7eab58fb9e0adc8bd704423
Tim Nelson <traveltimn@gmail.com>
2019-04-16 14:26:10 +0100

    Add functionality to view Recipes
    Basic functionality to view all recipte together,
    or individually view a particular recipe.
    If no image URL provided by user, a generic image appears.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,33 @@
 from app import app
+from datetime import datetime
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from slugify import slugify
 from flask import Flask, render_template, redirect, request, url_for, flash
 
 mongo = PyMongo(app)
 
-# index.html
+# database collections
+allergens_coll = mongo.db.allergens
+desserts_coll = mongo.db.desserts
+recipes_coll = mongo.db.recipes
+users_coll = mongo.db.users
+
+# HOME
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("base.html")
+
+
+# VIEW all desserts
+@app.route("/desserts")
+def view_desserts():
+    return render_template("view_desserts.html", recipes = recipes_coll.find())
+
+
+# VIEW single dessert
+@app.route("/dessert/<recipe_id>/<slugUrl>")
+def view_dessert(recipe_id, slugUrl):
+    recipe_name = recipes_coll.find_one({"_id": ObjectId(recipe_id)})
+    allergen = allergens_coll.find
+    return render_template("view_dessert.html", recipe_name = recipe_name, recipes = recipes_coll.find())
