9b82a515e322d5ac51290d06c3d71da7fb4ad6aa
Tim Nelson <traveltimn@gmail.com>
2019-05-22 23:13:39 +0100

    Add Python dotenv and and Flask-Talisman
    I have moved my environment variables out of my locally hosted
    nano file, and into a .env file instead. I have also included
    a .env_sample file which I will use in my readme for future users.
    
    In addition to the python dotenv, I have added Flask-Talisman
    for security headers, which force the site to load HTTPS now.
    
    The requirements.txt file has also been updated accordingly.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
 import os
 from flask import Flask
+from flask_talisman import Talisman
 
 # app initialization + configuration
 app = Flask(__name__)
+
+# whitelist sources on app for Talisman
+csp = {
+    "img-src": "*",
+    "default-src": [
+        '\'self\'',
+        "*.cloudflare.com",
+        "*.fontawesome.com",
+        "*.googleapis.com",
+        "*.gstatic.com"
+    ],
+    "script-src": [
+        '\'self\'',
+        "*.jquery.com",
+        "*.cloudflare.com"
+    ]
+}
+# force HTTPS security header using Flask-Talisman
+Talisman(app, content_security_policy=csp)
+
 app.config["MONGO_DBNAME"] = "2BN-Desserts"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
