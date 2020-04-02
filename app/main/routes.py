c850844bae86c7e5725258ed072b06e65e22b30c
Tim Nelson <traveltimn@gmail.com>
2020-04-02 21:41:17 +0100

    Testing real IP address - not Heroku IP

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,7 +25,9 @@ def home():
 
     """ Add site user to list of visitors. """
 
-    client_ip = request.access_route[0]
+    # client_ip = request.access_route[0]
+    headers_list = request.headers.getlist("X-Forwarded-For")
+    client_ip = headers_list[-1] if headers_list else request.remote_addr
     # trusted_proxies = {"127.0.0.1", "192.168.1.185"}
     # route = request.access_route + [request.remote_addr]
     # remote_addr = next(
