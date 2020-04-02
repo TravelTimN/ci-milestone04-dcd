bf8baccdddc8e92154fdadd37b658b3141600961
Tim Nelson <traveltimn@gmail.com>
2020-04-02 21:26:25 +0100

    Testing real IP address - not Heroku IP

--- app/main/routes.py
+++ app/main/routes.py
@@ -24,33 +24,13 @@ def home():
             {"$sample": {"size": 8}}])])
 
     """ Add site user to list of visitors. """
-    # http://httpbin.org/ip
-    client_ip = request.access_route[-1]
-    # if not ip.startswith("192."):
-    # print(ip)
-    # if request.headers.get("X-Forwarded-For"):
-    #     ip = request.headers.getlist("X-Forwarded-For")[-1]
-        #------ url = "https://ipapi.co/" + ip + "json/"
-    # # else:
-    #     # ip = request.remote_addr
-    # # print(ip)
-        #------- response = requests.get(url).json()
-    #     # response = requests.get("https://ipapi.co/" + ip + "json/").json()
-    #     # print(response)
-        #------ if response:
-    #         # ip = response["ip"]
-    #         # check if existing ip visitor already exists
-            # if visitors_collection.count_documents({"ip": ip}, limit=1) == 0:
-            #     submit = {
-            #         "ip": ip,
-            #         "city": response["city"],
-            #         "region": response["region"],
-            #         "country": response["country_name"],
-            #         "latitude": response["latitude"],
-            #         "longitude": response["longitude"],
-            #         "timezone": response["timezone"],
-            #         "utc_offset": response["utc_offset"]
-            #     }
-            #     visitors_collection.insert_one(submit)
 
-    return render_template("index.html", carousel=carousel, ip=client_ip)
+    # client_ip = request.access_route[-1]
+    trusted_proxies = {"127.0.0.1", "192.168.1.185"}
+    route = request.access_route + [request.remote_addr]
+    remote_addr = next(
+        (addr for addr in reversed(route)
+            if addr not in trusted_proxies), request.remote_addr)
+    print(remote_addr)
+
+    return render_template("index.html", carousel=carousel, ip=remote_addr)
