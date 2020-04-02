8db407cae79ae539a2fb0ba156a15de41717c110
Tim Nelson <traveltimn@gmail.com>
2020-04-02 21:37:43 +0100

    Testing real IP address - not Heroku IP

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, render_template, request
 from app import mongo
 from app.utils import recipes_collection, visitors_collection
 import requests
@@ -25,12 +25,12 @@ def home():
 
     """ Add site user to list of visitors. """
 
-    # client_ip = request.access_route[-1]
-    trusted_proxies = {"127.0.0.1", "192.168.1.185"}
-    route = request.access_route + [request.remote_addr]
-    remote_addr = next(
-        (addr for addr in reversed(route)
-            if addr not in trusted_proxies), request.remote_addr)
-    print(remote_addr)
+    client_ip = request.access_route[0]
+    # trusted_proxies = {"127.0.0.1", "192.168.1.185"}
+    # route = request.access_route + [request.remote_addr]
+    # remote_addr = next(
+    #     (addr for addr in reversed(route)
+    #         if addr not in trusted_proxies), request.remote_addr)
+    # print(remote_addr)
 
-    return render_template("index.html", carousel=carousel, ip=remote_addr)
+    return render_template("index.html", carousel=carousel, ip=client_ip)
