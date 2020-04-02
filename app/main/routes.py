3008b7d59119c1706880c9b025f4d6e9babef806
Tim Nelson <traveltimn@gmail.com>
2020-04-02 21:14:10 +0100

    Testing real IP address - not Heroku IP

--- app/main/routes.py
+++ app/main/routes.py
@@ -26,31 +26,31 @@ def home():
     """ Add site user to list of visitors. """
     # http://httpbin.org/ip
     ip = request.access_route[-1]
-    if not ip.startswith("192."):
+    # if not ip.startswith("192."):
     # print(ip)
     # if request.headers.get("X-Forwarded-For"):
     #     ip = request.headers.getlist("X-Forwarded-For")[-1]
-        url = "https://ipapi.co/" + ip + "json/"
+        #------ url = "https://ipapi.co/" + ip + "json/"
     # # else:
     #     # ip = request.remote_addr
     # # print(ip)
-        response = requests.get(url).json()
+        #------- response = requests.get(url).json()
     #     # response = requests.get("https://ipapi.co/" + ip + "json/").json()
     #     # print(response)
-        if response:
+        #------ if response:
     #         # ip = response["ip"]
     #         # check if existing ip visitor already exists
-            if visitors_collection.count_documents({"ip": ip}, limit=1) == 0:
-                submit = {
-                    "ip": ip,
-                    "city": response["city"],
-                    "region": response["region"],
-                    "country": response["country_name"],
-                    "latitude": response["latitude"],
-                    "longitude": response["longitude"],
-                    "timezone": response["timezone"],
-                    "utc_offset": response["utc_offset"]
-                }
-                visitors_collection.insert_one(submit)
+            # if visitors_collection.count_documents({"ip": ip}, limit=1) == 0:
+            #     submit = {
+            #         "ip": ip,
+            #         "city": response["city"],
+            #         "region": response["region"],
+            #         "country": response["country_name"],
+            #         "latitude": response["latitude"],
+            #         "longitude": response["longitude"],
+            #         "timezone": response["timezone"],
+            #         "utc_offset": response["utc_offset"]
+            #     }
+            #     visitors_collection.insert_one(submit)
 
-    return render_template("index.html", carousel=carousel)
+    return render_template("index.html", carousel=carousel, ip=ip)
