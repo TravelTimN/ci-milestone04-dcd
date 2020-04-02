0d5b7188f6de5097ecfa337fd8209bf6625b8a49
Tim Nelson <traveltimn@gmail.com>
2020-04-02 22:38:57 +0100

    Testing real IP address - not Heroku IP

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template, request
 from app import mongo
 from app.utils import recipes_collection, visitors_collection
+import os
 import requests
 
 
@@ -24,24 +25,13 @@ def home():
             {"$sample": {"size": 8}}])])
 
     """ Add site user to list of visitors. """
-
-
-    # http://httpbin.org/ip
     client_ip = request.access_route[-1]
-    if not client_ip.startswith("192."):
-    # print(ip)
-    # if request.headers.get("X-Forwarded-For"):
-    #     ip = request.headers.getlist("X-Forwarded-For")[-1]
-        url = "https://ipapi.co/" + client_ip + "json/"
-    # # else:
-    #     # ip = request.remote_addr
-    # # print(ip)
+    if not os.environ.get("DEVELOPMENT"):
+        # http://httpbin.org/ip
+        url = "https://ipapi.co/" + client_ip + "/json/"
         response = requests.get(url).json()
-    #     # response = requests.get("https://ipapi.co/" + ip + "json/").json()
-    #     # print(response)
         if response:
-    #         # ip = response["ip"]
-    #         # check if existing ip visitor already exists
+            # check if existing ip visitor already exists
             if visitors_collection.count_documents({"ip": client_ip}, limit=1) == 0:
                 submit = {
                     "ip": client_ip,
@@ -54,15 +44,5 @@ def home():
                     "utc_offset": response["utc_offset"]
                 }
                 visitors_collection.insert_one(submit)
-
-    # client_ip = request.access_route[0]
-    # headers_list = request.headers.getlist("X-Forwarded-For")
-    # client_ip = headers_list[-1] if headers_list else request.remote_addr
-    # trusted_proxies = {"127.0.0.1", "192.168.1.185"}
-    # route = request.access_route + [request.remote_addr]
-    # remote_addr = next(
-        # (addr for addr in reversed(route)
-            # if addr not in trusted_proxies), request.remote_addr)
-    # print(remote_addr)
 
     return render_template("index.html", carousel=carousel, ip=client_ip)
